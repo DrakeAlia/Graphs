d4c01a4cd90f2a7965bbcdb74e8fa1ebd4814dc7
Drake Alia <DrakeTesla@macbook-pro.lan>
2020-08-11 09:10:30 -0700

    added graph class to ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,20 @@
+class Graph:
 
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, vertex_id):
+        """
+        Add a vertex to the graph.
+        """
+        self.vertices[vertex_id] = set()
+
+    def add_edge(self, v1, v2):
+        """
+        Add a directed edge to the graph.
+        """
+        self.vertices[v1].add(v2)
+        
 def earliest_ancestor(ancestors, starting_node):
     pass
--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -173,43 +173,6 @@ class Graph:
                     new_path.append(neighbor)
                     q.enqueue(new_path)
 
-    # def bfs(self, starting_vertex, destination_vertex):
-    #     """
-    #     Return a list containing the shortest path from
-    #     starting_vertex to destination_vertex in
-    #     breath-first order.
-    #     """
-    #     # pass  # TODO
-    #     # create an empty queue and enqueue A PATH to the starting vertex 
-    #     q = Queue()
-    #     q.enqueue([starting_vertex])
-    #     # create a SET to store visited vertices
-    #     visisted = set()
-
-    #     # while the queue is not empty
-    #     while q.size() > 0:
-    #         # dequeue the first path (front or back?)
-    #         current_path = q.dequeue()
-    #         # Grab the last vertex from the PATH
-    #         current_vertex = current_path[-1]
-
-    #         # if that vertex has not been visited
-    #         if current_vertex not in visisted:
-    #             # check if it's the target
-    #             if current_vertex == destination_vertex:
-    #                 # if so, return path
-    #                 return current_path
-    #             # Mark as visited
-    #             visisted.add(current_vertex)
-    #             # add PATHs neighbors to the back of the queue
-    #             neighbors = self.get_neighbors(current_vertex)
-    #             for neighbor in neighbors:
-    #                 # copy the PATH
-    #                 current_path.append(neighbor)
-    #                 print("Path:", current_path)
-    #                 # append neighbor to the back
-    #                 q.enqueue(current_path)
-
 
     def dfs(self, starting_vertex, destination_vertex):
         """
